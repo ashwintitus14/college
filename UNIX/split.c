b9def2487238a7ded7f6b181ca458991659186b2
Ashwin Titus <ashwintitus14@gmail.com>
2018-09-20 15:44:26 +0530

    Added functionality

--- UNIX/split.c
+++ UNIX/split.c
@@ -1,13 +1,40 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+#define line_max 200
+#define filename_max 100
 
+int split(char filename[], int size){
+	char line[line_max], generated_filename[10];
+	generated_filename[0] = 'x';
+	generated_filename[1] = '`';
+	int i = 0;
+		
+	FILE *fp, *fw;
+	fp = fopen(filename, "r");
+	while(fgets(line,200,fp) != NULL){
+		if(i == 0){	
+			generated_filename[1]++;
+			fw = fopen(generated_filename,"w+");
+		}
+		if(i<size){
+			fputs(line,fw);
+			i++;
+			if(i == size){
+				fclose(fw);
+				i = 0;
+			}
+		}
+		printf("%d\n",i);
+			
+	}
+	fclose(fp);
+	return 0;
+}
 int main(int argc, char* argv[]){
 	
-	char filename[100],generated_filename[10];
-	char line[200];
-	int size = 5,i = 0;		/*size: number of lines in each new file*/
-	generated_filename[0] = 'a';		
+	char filename[filename_max];
+	int size = 5;		/*size: number of lines in each new file*/
 	
 	switch(argc){
 		case 1:{
@@ -16,26 +43,24 @@ int main(int argc, char* argv[]){
 		}
 		case 2:{
 				strcpy(filename, argv[1]);
-				FILE *fp, *fw;
-				fp = fopen(filename, "r");
-				while(fgets(line,200,fp) != NULL){
-					if(i == 0){	
-						generated_filename[0]++;
-						fw = fopen(generated_filename,"w+");
-					}
-					if(i<size){
-						fputs(line,fw);
-						i++;
-						if(i == size){
-							fclose(fw);
-							i = 0;
-						}
-					}
-				printf("%d\n",i);	
-				}
-				fclose(fp);
+				split(filename, size);
 				break;
 		}
+		case 3:{
+			       printf("invalid number of lines: %s\n",argv[2]);
+			       break;
+		}
+		case 4:{
+			       if(strcmp(argv[1],"-l") == 0){
+				       size = atoi(argv[2]);
+				      strcpy( filename,argv[3]);
+				}
+			       split(filename, size);
+			       break;
+		}
+		default:{
+				printf("Too many arguments!\n");
+		}	
 	}
 	return 0;
 }
