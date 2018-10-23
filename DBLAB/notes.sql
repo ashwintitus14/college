f88c7375cec282eee00991bb1c2cadef9bf5ac7c
Ashwin Titus <ashwintitus14@gmail.com>
2018-10-23 11:29:51 +0530

    Update notes.sql

--- DBLAB/notes.sql
+++ DBLAB/notes.sql
@@ -10,4 +10,19 @@ CREATE TABLE class_(
   class_id SERIAL PRIMARY KEY,
   class_name VARCHAR(30) NOT NULL,
   division VARCHAR(2) NOT NULL DEFAULT 'A',
-  st_cnt INT NOT NULL CHECK(st_cnt >= 0)
+  st_cnt INT NOT NULL CHECK(st_cnt >= 0),
+  UNIQUE( class_name, division)
+ );
+ 
+ INSERT INTO class_(class_name, st_cnt)
+ VALUES ('Ten', 65);
+ 
+ CREATE TABLE student(
+   st_id SERIAL PRIMARY KEY,
+   st_fname VARCHAR(30) NOT NULL,
+   st_lname VARCHAR(30) NOT NULL,
+   addr VARCHAR(30) NOT NULL,
+   phone VARCHAR(13),
+   email VARCHAR(30),
+   class_id INT REFERENCES class_(class_id)
+  );
