e6e60e1f285cd8aff55cfe31020250eec2a0f4ae
Ashwin Titus <ashwintitus14@gmail.com>
2018-10-23 11:36:47 +0530

    Update and rename notes.sql to student.sql

--- DBLAB/student.sql
+++ DBLAB/student.sql
@@ -1,9 +1,22 @@
 /*
+23-10-18
+
 STUDENT DATABASE
 class_(class_id, clas_name, division, st_cnt)
 student(st_id, st_fname, st_lname, addr, phone, email, class_id)
 teacher(teacher_id, fname, lname, phone, subject)
 student_class(st_id, class_id, teacher_id)
+
+1) Create the tables and insert the values,
+2) List all classes where strength is greater than 50
+3) List the name of students of Lina teacher.
+4) List the names of all the English teachers.
+5) List the names of teachers who teach standard 9.
+6) Find out all the classes that are taught by Jaya teacher.
+7) List the names and details of all students in standard 10.
+8) List all the students whose first name is the same along with their student id.
+9) List the names of students whose name starts with 's'.
+
 */
 
 CREATE TABLE class_(
