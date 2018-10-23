6ee1a5cea7e6b7b453565b88a0168182bc099043
Ashwin Titus <ashwintitus14@gmail.com>
2018-10-23 11:19:36 +0530

    Create notes.sql

---
+++ DBLAB/notes.sql
@@ -0,0 +1,8 @@
+/*
+STUDENT DATABASE
+class(class_id, clas_name, division, st_cnt)
+student(st_id, st_fname, st_lname, addr, phone, email, class_id)
+teacher(teacher_id, fname, lname, phone, subject)
+student_class(st_id, class_id, teacher_id)
+*/
+
