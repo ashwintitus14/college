2bdefe7a1b6b677e84afada6ffa8162fa5abf2e9
Ashwin Titus <ashwintitus14@gmail.com>
2018-10-23 11:23:10 +0530

    Update notes.sql

--- DBLAB/notes.sql
+++ DBLAB/notes.sql
@@ -1,8 +1,13 @@
 /*
 STUDENT DATABASE
-class(class_id, clas_name, division, st_cnt)
+class_(class_id, clas_name, division, st_cnt)
 student(st_id, st_fname, st_lname, addr, phone, email, class_id)
 teacher(teacher_id, fname, lname, phone, subject)
 student_class(st_id, class_id, teacher_id)
 */
 
+CREATE TABLE class_(
+  class_id SERIAL PRIMARY KEY,
+  class_name VARCHAR(30) NOT NULL,
+  division VARCHAR(2) NOT NULL DEFAULT 'A',
+  st_cnt INT NOT NULL CHECK(st_cnt >= 0)
