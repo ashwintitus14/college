97ec4bf945fe6f5939695692669b316b7af4bfc2
Ashwin Titus <ashwintitus14@gmail.com>
2018-10-30 11:21:49 +0530

    Update function.sql

--- DBLAB/function.sql
+++ DBLAB/function.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE FUNCTION inc(n INT)
 RETURNS INT AS 
 $BODY$
 BEGIN
-RETURN n+1;
+    RETURN n+1;
 END;
 $BODY$
 LANGUAGE plpgsql;
@@ -11,7 +11,28 @@ CREATE OR REPLACE FUNCTION add(n INT, m INT)
 RETURNS INT AS
 $BODY$
 BEGIN
-RETURN n+m;
+    RETURN n+m;
+END;
+$BODY$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION fib(n INT)
+RETURN INT AS
+$BODY$
+DECLARE
+    prev INT = 0;
+    curr INT = 1;
+    next INT;
+    cnt INT = 0;
+BEGIN
+    LOOP
+    EXIT WHEN cnt = n-1;
+        next = prev+curr;
+        prev = curr;
+        curr = next;
+        cnt = cnt+1;
+    END LOOP;
+    RETURN prev;
 END;
 $BODY$
 LANGUAGE plpgsql;
