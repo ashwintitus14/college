a3d0f815812cc5672764cfd2e7e18a298c3605f0
Ashwin Titus <ashwintitus14@gmail.com>
2019-03-25 12:23:39 +0530

    Add files via upload

---
+++ IT Lab/JS/fibscript.js
@@ -0,0 +1,18 @@
+function fib() {
+	var n = document.getElementById("n").value;
+	
+	function fibrec(n) {
+		if (n==0 || n==1) {
+			return n;
+		}
+		else {
+			return fibrec(n-1) + fibrec(n-2);
+		}
+	}
+	var i;
+	for (i=0; i <= n; i++) {
+		document.write(fibrec(i)+'<br>');
+		//document.getElementById("result").innerHTML=fibrec(n);
+	
+	}
+}
