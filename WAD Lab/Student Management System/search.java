4def7fb5402b6e333b0565664c11da727bcb7062
Ashwin Titus <ashwintitus14@gmail.com>
2019-09-17 14:06:31 +0530

    Add files via upload

---
+++ WAD Lab/Student Management System/search.java
@@ -0,0 +1,133 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.sql.*;
+
+/**
+ *
+ * @author wad15
+ */
+@WebServlet(urlPatterns = {"/search"})
+public class search extends HttpServlet {
+    
+    Connection connection;
+    String url = "jdbc:postgresql://localhost:5432/student";
+    String dbusername = "postgres";
+    String dbpassword = "root";
+    
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet create</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet create at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //processRequest(request, response);
+        PrintWriter out = response.getWriter();
+        
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch(ClassNotFoundException e) {
+            out.println(e.getMessage());
+        }
+        
+        try {
+            String name, dob, branch, rollno, _rollno;
+            int flag = 0;
+            _rollno = request.getParameter("rollno");
+                        
+            connection = DriverManager.getConnection(url, dbusername, dbpassword);
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from stud;");
+            out.println("<html><body>");
+            while(rs.next()) {
+                name = rs.getString("name");
+                dob = rs.getString("dob");
+                branch = rs.getString("branch");
+                rollno = rs.getString("rollno");
+                if (rollno.equals(_rollno)) {
+                    flag=1;
+                    out.println("<table><tr><th>Name</th><th>DOB</th><th>Branch</th><th>Roll No.</th></tr><tr><td>"+name+"</td><td>"+dob+"</td><td>"+branch+"</td><td>"+rollno+"</td></tr></table>");   
+                }             
+                
+            }
+            if(flag==0){
+                out.println("<h2>Record not found!</h2>");
+            }
+            connection.close();
+            
+            out.println("<a href='index.html'>Go back to main menu</a>");
+            out.println("</body></html>");
+        
+        } catch(SQLException ex) {
+            out.println(ex.getMessage());
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ WAD Lab/Student Management System/update.java
@@ -0,0 +1,131 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.sql.*;
+
+/**
+ *
+ * @author wad15
+ */
+@WebServlet(urlPatterns = {"/update"})
+public class update extends HttpServlet {
+    
+    Connection connection;
+    String url = "jdbc:postgresql://localhost:5432/student";
+    String dbusername = "postgres";
+    String dbpassword = "root";
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet update</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet update at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //processRequest(request, response);
+        
+        PrintWriter out = response.getWriter();
+        
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch(ClassNotFoundException e) {
+            out.println(e.getMessage());
+        }
+        
+        try {
+            String name, dob, branch;
+            int rollno;
+            name = request.getParameter("name");
+            dob = request.getParameter("dob");
+            branch = request.getParameter("branch");
+            rollno = Integer.parseInt(request.getParameter("rollno"));
+            
+            connection = DriverManager.getConnection(url, dbusername, dbpassword);
+            Statement stmt = connection.createStatement();
+            //int i = stmt.executeUpdate("INSERT INTO stud(name, dob, branch, rollno) VALUES ('"+name+"','"+dob+"','"+branch+"','"+rollno+"');");
+            int i = stmt.executeUpdate("UPDATE stud SET name = '"+name+"', dob = '"+dob+"', branch = '"+branch+"' WHERE rollno = "+rollno+";");
+            
+            connection.close();
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<h2>Student record updated successfully!</h2>");
+            out.println("<a href='index.html'>Go back to main menu</a>");
+            out.println("</html>");
+        
+        } catch(SQLException ex) {
+            out.println(ex.getMessage());
+        }
+        
+        
+        
+        
+        
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
