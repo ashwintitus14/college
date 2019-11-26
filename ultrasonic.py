c53cec529acf67b38e644bab99ded53865b75dd5
Ashwin Titus <ashwintitus14@gmail.com>
2019-11-26 11:21:53 +0530

    Add files via upload

---
+++ ultrasonic.py
@@ -0,0 +1,44 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO_TRIG = 18
+GPIO_ECHO = 24
+
+GPIO.setup(GPIO_TRIG, GPIO.OUT)
+GPIO.setup(GPIO_ECHO, GPIO.IN)
+
+def distance():
+    GPIO.output(GPIO_TRIG, True)
+
+    time.sleep(0.00001) #0.01ms
+    GPIO.output(GPIO_TRIG, False)
+    
+    while GPIO.input(GPIO_ECHO) == 0:
+        starttime = time.time()
+
+    while GPIO.input(GPIO_ECHO) == 1:
+        stoptime = time.time()
+
+    timeelapsed = stoptime - starttime
+    distance = (timeelapsed * 34300) / 2
+
+    return distance
+
+
+screen_diagonal = input("Enter the diagonal length of the projector screen in inches")
+lcd_diagonal = input("Enter the diagonal length of the Projector's LCD Panel in inches")
+
+
+try:
+    while True:
+        dist = distance()
+        dist_inches = dist*0.393701
+        lens_focal_length = (dist * (lcd_diagonal*0.8))/ screen_diagonal
+        print("Focal Length of lens = %.1f cm" % lens_focal_length)
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    print("Measurement stopped by User")
+    GPIO.cleanup()
